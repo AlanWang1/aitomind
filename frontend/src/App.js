44b2489eca1f582c84b5256ac15de2609e7ad202
Alan Wang <alan.wang9018@gmail.com>
2023-06-22 01:02:54 -0400

    styling and UI element updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import MindMapPage from './pages/MindMapPage';
-import NavBar from './components/NavBar'
-import '@fortawesome/fontawesome-free/css/all.css'
-import './App.scss'
+import React from "react";
+import MindMapPage from "./pages/MindMapPage";
+import NavBar from "./components/NavBar";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "./App.scss";
 function App() {
   return (
     <div className="App">
-    <NavBar />
-    <MindMapPage/>
+      <NavBar />
+      <MindMapPage />
     </div>
   );
 }
